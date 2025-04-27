894bdc11303d4ec84b61a020a45c8a9aded779d2
Max Klenov <maxklenow@gmail.com>
2025-04-27 13:43:30 +0300

    published on github pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,21 @@
 import { defineConfig } from 'vite'
+import postcss from 'postcss';
+import autoprefixer from 'autoprefixer';
+import postcssPresetEnv from 'postcss-preset-env';
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  css: {
+    postcss: {
+      plugins: [
+        autoprefixer(),
+        postcssPresetEnv({
+          stage: 3,
+        }),
+      ],
+    },
+  },
+  base: '/currency-converter/',
 })
