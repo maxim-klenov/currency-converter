3581d7d5cf8c1c31e845f8c932c3f88b147b563b
Max Klenov <maxklenow@gmail.com>
2025-05-29 18:30:27 +0300

    type(feat): css modules

--- src/components/Dropdown/Dropdown.jsx
+++ src/components/Dropdown/Dropdown.jsx
@@ -1,9 +1,13 @@
-import CheckedIcon from "./CheckedIcon"
+import CheckedIcon from "../CheckedIcon"
+import styles from './Dropdown.module.css';
 export default function Dropdown({currency, setCurrency}) {
   const currencies = [
     { key: 'RUB', label: 'российский рубль'},
     { key: 'USD', label: 'доллар США'},
     { key: 'EUR', label: 'евро'},
+    { key: 'TRY', label: 'турецкая лира'},
+    { key: 'UAH', label: 'украинская гривна'},
+    { key: 'BYN', label: 'белорусский рубль'},
     { key: 'TMT', label: 'туркменский манат'},
     { key: 'JPY', label: 'японская иена'},
     { key: 'GBP', label: 'фунт Стерлинга'},
@@ -11,12 +15,12 @@ export default function Dropdown({currency, setCurrency}) {
     { key: 'ETH', label: 'ethereum'},
   ];
   return (
-    <div className='dropdown-menu'>
-      <ul className='dropdown-menu__list'>
+    <div className={styles["dropdown"]}>
+      <ul className={styles["list"]}>
       {currencies.map(({key, label}) => (
-        <li key={key} className="dropdown-menu__item" onClick={() => setCurrency(key)}>
-          <span className='dropdown-menu__item-currency'>{label}</span>
-          <span className='dropdown-menu__item-icon'>
+        <li key={key} className={styles["list__item"]} onClick={() => setCurrency(key)}>
+          <span className={styles["list__item-currency"]}>{label}</span>
+          <span className={styles["list__item-icon"]}>
             {currency === key && <CheckedIcon />}
           </span>
         </li>
